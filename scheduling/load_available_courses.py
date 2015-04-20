6e4143d16340cc020bbeb7a1f1cb3186dcf79be8
Jamie Fujimoto <hayntopdawg@gmail.com>
2015-04-20 15:57:25 -0400

    Add comments

--- scheduling/load_available_courses.py
+++ scheduling/load_available_courses.py
@@ -16,7 +16,7 @@ def load_available_courses(csv_file_name):
     return available_crn
 
 def parse_time(days, start_time, end_time):
-    # Need to convert times from 12 hr to 24 hr
+    # Convert times from 12 hr to 24 hr
     start_time = convert_time(start_time)
     end_time = convert_time(end_time)
  
@@ -40,16 +40,14 @@ def parse_time(days, start_time, end_time):
 def parse_csv(csv_file):
     crn = []
     
-    # Should add a way to skip the header line
     for row in csv_file:
-        # Make sure AM and PM are in correct format
-        # Assume that if meridian is not 'AM' or 'PM'
-        #     set to 'AM'
+        # If meridian is not 'AM' or 'PM' set to 'AM'
         m = row['']
         if m != 'AM' and m != 'PM':
             m = 'AM'
 
-        # Make sure HH:MM is in the correct format
+        # If not in HH:MM format set to empty string
+        # Need to correct.  Currently only assumes TBA as only bad input.
         st = row['start_time']
         if st == '** TBA **':
             st = ''
@@ -59,7 +57,7 @@ def parse_csv(csv_file):
         
         ts = parse_time(row['days'], st + m, et + m)
 
-        # Create a new course
+        # Create course
         # Initializing with no prereqs (need database with prereqs)
         c = Course(row['crn?'], ts, None, row['credit_hours'])
         
@@ -67,20 +65,22 @@ def parse_csv(csv_file):
 
     return crn
 
-# There is an issue '10:00AM' & '11:00AM' times.
-# Printing out as '010:00' & '011:00'
 def convert_time(time):
+    # Assumes time is in one of the following formats H:MMAM, H:MMPM,
+    #     HH:MMAM, HH:MMPM
     if time[-2:] == 'AM':
+        # Checks if hours is 2 digits, then truncates meridian
         if len(time.split(':')[0]) == 2:
-            time = time[-len(time):-2]
+            time = time.split('AM')[0]
+        # If hour is 1 digit, add 0 before hour and truncate meridian
         else:
-            time = '0' + time[-len(time):-2]
+            time = '0' + time.split('AM')[0]
+    # Changes PM times to correct 24 hr format and truncates meridian
     elif time[-2:] == 'PM':
-        size = len(time)
-        hours = int(time[-size:size - 5])
+        hours = int(time.split(':')[0])
         if hours != 12:
             hours += 12
-        time = str(hours) + time[-5:-2]
+        time = str(hours) + ':' + time.split(':')[1].split('PM')[0]
     return time
 
 if __name__ == "__main__":
