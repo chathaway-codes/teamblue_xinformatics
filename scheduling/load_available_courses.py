0258ebb04cdf676b61f21c4d155f2fe66c1b0ba1
Jamie Fujimoto <hayntopdawg@gmail.com>
2015-04-20 15:23:09 -0400

    Fix time formatting

--- scheduling/load_available_courses.py
+++ scheduling/load_available_courses.py
@@ -15,14 +15,13 @@ def load_available_courses(csv_file_name):
 
     return available_crn
 
-def parse_time(start_time, end_time):
+def parse_time(days, start_time, end_time):
     # Need to convert times from 12 hr to 24 hr
-    start_time = convert_time(start_time + m)
-    end_time = convert_time(end_time + m)
+    start_time = convert_time(start_time)
+    end_time = convert_time(end_time)
  
     # Read in the times for the course
     d = {}
-    days = row['days']
     if 'M' in days:
         d['M'] = [start_time, end_time]
     if 'T' in days:
@@ -37,8 +36,6 @@ def parse_time(start_time, end_time):
     # Create timeslot for the course
     # Hard-coded semester (should be read in somehow)
     return Timeslot("FA14", d)
-
-    
 
 def parse_csv(csv_file):
     crn = []
@@ -60,7 +57,7 @@ def parse_csv(csv_file):
         if et == '** TBA **':
             et = ''
         
-        ts = parse_time(st, et)
+        ts = parse_time(row['days'], st + m, et + m)
 
         # Create a new course
         # Initializing with no prereqs (need database with prereqs)
@@ -74,7 +71,10 @@ def parse_csv(csv_file):
 # Printing out as '010:00' & '011:00'
 def convert_time(time):
     if time[-2:] == 'AM':
-        time = '0' + time[-len(time):-2]
+        if len(time.split(':')[0]) == 2:
+            time = time[-len(time):-2]
+        else:
+            time = '0' + time[-len(time):-2]
     elif time[-2:] == 'PM':
         size = len(time)
         hours = int(time[-size:size - 5])
@@ -87,6 +87,6 @@ if __name__ == "__main__":
     load_available_courses('sis_class_schedule_fall_2014_CSCI.csv')
 
 ### For testing purposes
-##available_crn = load_available_courses()
-##for c in available_crn:
-##    print c.CRN, c.timeslot.semester, c.timeslot.day_time, c.prereqs, c.credit_hours
+available_crn = load_available_courses('sis_class_schedule_fall_2014_CSCI.csv')
+for c in available_crn:
+    print c.CRN, c.timeslot.semester, c.timeslot.day_time, c.prereqs, c.credit_hours
