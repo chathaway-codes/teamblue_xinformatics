82ea3ef257f3a235144432c514c91e3e97305e77
Charles Hathaway <chathaway@logrit.com>
2015-04-28 11:30:26 -0400

    Added in CORS headers

--- course_wizard/settings.py
+++ course_wizard/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'course_wizard',
+    'corsheaders',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -97,6 +99,8 @@ STATICFILES_DIRS = (
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, '/templates/'),
 )
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Import local settings
 try:
